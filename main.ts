1b1418fa81a3bb924fd0a0fdafee1ba9931e6085
Marcos Quílez <marcos.quilez@upc.edu>
2021-06-16 16:22:47 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.OnceInBackground)
+    basic.showIcon(IconNames.No)
+})
 basic.clearScreen()
